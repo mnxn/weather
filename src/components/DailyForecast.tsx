59a1bf8051706b83e6a62fe45ef7d4156be4f129
Max Lantas <mnxndev@outlook.com>
2023-11-11 17:36:55 -0800

    daily forecast component

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -1,18 +1,53 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Paper, Stack, Tooltip } from "@mui/material";
+import { WmoCode, weatherDescription, weatherIconClass } from "./WmoCode";
+import { blue, red } from "@mui/material/colors";
 
-function DailyForecast(props: { id?: string }) {
+type DailyForecastProps = {
+  date: number;
+  day: string;
+  weather: WmoCode;
+  high: number;
+  low: number;
+  id?: string;
+};
+
+function DailyForecast(props: DailyForecastProps) {
   return (
-    <Box
-      id={props.id}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-      bgcolor="white"
-      borderRadius={1}
-    >
-      Daily Forecast
-    </Box>
+    <Paper elevation={1} sx={{ flex: 1 }}>
+      <Stack
+        id={props.id}
+        alignItems="center"
+        textAlign="center"
+        paddingBlock={1}
+      >
+        {props.date}
+        <h3 style={{ margin: 0 }}>{props.day}</h3>
+        <Box
+          className={`wi ${weatherIconClass(props.weather)}`}
+          fontSize={50}
+          alignSelf="stretch"
+          bgcolor={blue[100]}
+          color="black"
+          textAlign="center"
+          paddingBlock={2}
+          marginBlock={1}
+        />
+        <Box>{weatherDescription(props.weather)}</Box>
+        <Stack direction="row" gap={1} marginBlock={1} alignItems="center">
+          <Tooltip title="High Temperature">
+            <Box fontWeight="bold" color={red[500]}>
+              {props.high}&deg;
+            </Box>
+          </Tooltip>
+          <Divider orientation="vertical" flexItem />
+          <Tooltip title="Low Temperature">
+            <Box fontWeight="bold" color={blue[500]}>
+              {props.low}&deg;
+            </Box>
+          </Tooltip>
+        </Stack>
+      </Stack>
+    </Paper>
   );
 }
 
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,23 +6,68 @@ import DailyForecast from "./DailyForecast";
 import HistoryPreview from "./HistoryPreview";
 
 import "./HomePage.css";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import { WmoCode } from "./WmoCode";
 
 function HomePage() {
   return (
     <Container>
-      <Box id="dashboard" display="grid" gap={2} padding={2} height={600}>
+      <Box id="dashboard" display="grid" gap={2} padding={2}>
         <CurrentLocation id="current-location" />
         <CurrentWeather id="current-weather" />
         <Map id="map" />
         <HourlyForecast id="hourly-forecast" />
-        <DailyForecast />
-        <DailyForecast />
-        <DailyForecast />
-        <DailyForecast />
-        <DailyForecast />
-        <DailyForecast />
-        <DailyForecast />
+        <Stack id="daily-forecast" direction="row" gap={1}>
+          <DailyForecast
+            date={11}
+            day="Sunday"
+            weather={WmoCode.ClearSky}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={12}
+            day="Monday"
+            weather={WmoCode.RainHeavy}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={13}
+            day="Tuesday"
+            weather={WmoCode.SnowHeavy}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={14}
+            day="Wednesday"
+            weather={WmoCode.Fog}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={15}
+            day="Thursday"
+            weather={WmoCode.DrizzleDense}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={16}
+            day="Friday"
+            weather={WmoCode.ThunderstormHailHeavy}
+            high={100}
+            low={50}
+          />
+          <DailyForecast
+            date={17}
+            day="Saturday"
+            weather={WmoCode.FreezingRainHeavy}
+            high={100}
+            low={50}
+          />
+        </Stack>
         <HistoryPreview id="history-preview" />
       </Box>
     </Container>
