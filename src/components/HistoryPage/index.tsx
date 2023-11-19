e8d8c871b5a2fd857e743d104c8de3e0ef90ce45
Max Lantas <mnxndev@outlook.com>
2023-11-19 00:21:44 -0800

    sunset history component

--- src/components/HistoryPage/index.tsx
+++ src/components/HistoryPage/index.tsx
@@ -1,10 +1,26 @@
 import { useEffect, useState } from "react";
-import { fetchHistoricalWeatherData } from "../../api/OpenMeteo";
+import {
+  SunsetData,
+  fetchHistoricalWeatherData,
+  fetchSunsetData,
+} from "../../api/OpenMeteo";
 import Box from "@mui/material/Box";
 import { FormattedData, formatChartData } from "./CalcHistory";
 import { PieChartContainer } from "./PieChart";
 import { BarChartContainer } from "./BarChart";
+import SunsetHistory from "./SunsetHistory";
 
+// Generic function to return every Nth element of an array.
+// Can be used to shrink a years worth of daily data to values every N days.
+function everyNth<T>(array: T[] | undefined, n: number): T[] {
+  if (array === undefined) {
+    return [];
+  }
+
+  return array.filter((_, index) => index % n == 0);
+}
+
+const DAYS: number = 7;
 
 const HistoryPage = () => {
   const [chartData, setChartData] = useState<FormattedData>({
@@ -19,9 +35,15 @@ const HistoryPage = () => {
     },
   });
 
+  const [sunsetData, setSunsetData] = useState<SunsetData | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const weatherData = await fetchHistoricalWeatherData(45.5152, -122.676483, 2023);
+      const weatherData = await fetchHistoricalWeatherData(
+        45.5152,
+        -122.676483,
+        2023
+      );
       const formattedData = formatChartData(weatherData);
       setChartData(formattedData);
     };
@@ -29,13 +51,21 @@ const HistoryPage = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchSunsetData(45.52345, -122.67621, 2022);
+      setSunsetData(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Box sx={{ p: 6 }}>
       <BarChartContainer
         title="Highest/lowest temperature"
         labels={chartData.monthly.labels}
-        datasets = {[
+        datasets={[
           {
             label: `Highest Temperature (°C)`,
             data: chartData.monthly.highestTemps,
@@ -49,13 +79,19 @@ const HistoryPage = () => {
             backgroundColor: "rgba(54, 162, 235, 0.5)",
             borderColor: "rgba(54, 162, 235, 1)",
             borderWidth: 1,
-          }
+          },
         ]}
       />
       <PieChartContainer
         title="Weather Distribution"
         labels={chartData.weatherDistribution.labels}
         data={chartData.weatherDistribution.data}
+      />
+
+      <SunsetHistory
+        times={everyNth(sunsetData?.time, DAYS)}
+        sunrise={everyNth(sunsetData?.sunrise, DAYS)}
+        sunset={everyNth(sunsetData?.sunset, DAYS)}
       />
     </Box>
   );
