fbe4ce0631b6cddc389d08e824663309085c33eb
ellie <elaheh@pdx.edu>
2023-11-21 17:08:43 -0500

    Changed HistoyPage to reflect changes in Calchistory

--- src/components/HistoryPage/HistoryPage.tsx
+++ src/components/HistoryPage/HistoryPage.tsx
@@ -1,37 +1,14 @@
 import { useEffect, useState } from "react";
-import { fetchAllWeather } from "../../api/OpenWeather";
-import {
-  ChartOptions,
-  Chart as ChartJS,
-  Title,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  ArcElement,
-} from "chart.js";
+import { fetchHistoricalWeatherData } from "../../api/OpenMeteo";
 import Box from "@mui/material/Box";
 import { FormattedData, formatChartData } from "./CalcHistory";
 import { PieChartContainer } from "./PieChart";
 import { BarChartContainer } from "./BarChart";
 
-ChartJS.register(
-  Title,
-  Legend,
-  ArcElement,
-  CategoryScale,
-  LinearScale,
-  BarElement
-);
 
 const HistoryPage = () => {
   const [chartData, setChartData] = useState<FormattedData>({
     monthly: {
-      labels: [],
-      highestTemps: [],
-      lowestTemps: [],
-    },
-    daily: {
       labels: [],
       highestTemps: [],
       lowestTemps: [],
@@ -44,51 +21,41 @@ const HistoryPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const forcastByCity = await fetchAllWeather(45.5152, -122.676483);
-      console.log("fetchAllWeather:", forcastByCity);
-
-      const formattedData = formatChartData(forcastByCity);
+      const weatherData = await fetchHistoricalWeatherData(45.5152, -122.676483, 2023);
+      const formattedData = formatChartData(weatherData);
       setChartData(formattedData);
     };
 
     fetchData();
   }, []);
 
-  const chartOptions: ChartOptions = {
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        position: "bottom" as const,
-        display: true,
-      },
-    },
-    animation: {
-      duration: 1000,
-      easing: "easeInOutQuart",
-    },
-  };
 
   return (
     <Box sx={{ p: 6 }}>
-      {/* <BarChartContainer
-        title="Monthly highest/lowest temperature"
-        labels={chartData.monthly.labels}
-        highestTemps={chartData.monthly.highestTemps}
-        lowestTemps={chartData.monthly.lowestTemps}
-        options={chartOptions}
-      /> */}
       <BarChartContainer
         title="Highest/lowest temperature"
-        labels={chartData.daily.labels}
-        highestTemps={chartData.daily.highestTemps}
-        lowestTemps={chartData.daily.lowestTemps}
-        options={chartOptions}
+        labels={chartData.monthly.labels}
+        datasets = {[
+          {
+            label: `Highest Temperature (°C)`,
+            data: chartData.monthly.highestTemps,
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: `Lowest Temperature (°C)`,
+            data: chartData.monthly.lowestTemps,
+            backgroundColor: "rgba(54, 162, 235, 0.5)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          }
+        ]}
       />
       <PieChartContainer
         title="Weather Distribution"
         labels={chartData.weatherDistribution.labels}
         data={chartData.weatherDistribution.data}
-        options={chartOptions}
       />
     </Box>
   );
